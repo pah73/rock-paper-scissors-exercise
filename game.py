c5646b2f05ac6905de4459db9dfe7654e948c9b3
Paul Hadchiti <pah73@georgetown.edu>
2021-02-06 17:13:03 -0500

    finished game conditions

--- game.py
+++ game.py
@@ -11,12 +11,11 @@ print("-------------------")
 # asking user for an input
 #
 user_input = input("Please choose either 'rock', 'paper', or 'scissors': ")
-print("You chose: '" , user_input , "'")
+print("You chose: '", user_input, "'")
 
 #
 #simulating a computer input
 #
-
 options = ["rock", "paper", "scissors"]
 computer_choice = random.choice(options)
 print("The computer chose: '" , computer_choice , "'")
@@ -26,8 +25,6 @@ print("-------------------")
 #determining who won
 #
 
-#draw condition
-
 if user_input == computer_choice :
     print("It's a tie !")
 elif user_input == "rock" and computer_choice == "scissors":
@@ -36,9 +33,12 @@ elif user_input == "scissors" and computer_choice == "paper":
         print("You win!")
 elif user_input == "paper" and computer_choice == "rock":
         print("You win!")
-
-
-
+elif computer_choice == "rock" and user_input == "scissors" :
+        print("Oh, the computer won. It's ok.")
+elif computer_choice == "scissors" and user_input == "paper" :
+        print("Oh, the computer won. It's ok.")
+elif computer_choice == "paper" and user_input == "rock" :
+        print("Oh, the computer won. It's ok.")
 
 print("-------------------")
 print("Thanks for playing. Please play again!")
